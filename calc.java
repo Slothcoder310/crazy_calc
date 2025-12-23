0cf3d63499a46cd22f9a1529b7dc7f149be86409
Hemanth-208 <2410030208@klh.edu.in>
2025-12-23 13:47:02 +0530

    Fix typo in multiplication output message
    added the multiplication

--- calc.java
+++ calc.java
@@ -2,5 +2,6 @@ public class calc {
     public static void main(String[] args) {
         int a = 10, b = 5;
         System.out.println("Addition: " + (a + b));
+        System.out.println("mmultiplication " + (a*b));
     }
 }
